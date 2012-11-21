ed109d77f85aa0cfb89f00e5593a396c15cb6024
suisho <suisho000000@gmail.com>
2012-11-21 21:06:55 +0900

    Add ignore word function

--- app/socket.js
+++ app/socket.js
@@ -1,5 +1,6 @@
 //socket
 var socket_io = require('socket.io');
+var config = require('./config');
 
 var io = null;
 
@@ -8,18 +9,41 @@ var _blockTailing = {};
 function setBlock(target){
   setTimeout(function(){
     _blockTailing[target] = false;
-  },3000);
+  },5000);
   _blockTailing[target] = true;
 }
+function hasIgnoreWord(){
 
+}
 exports.tail = function(data){
-  // 1秒程度notificationをブロックする。
-  // ブロッキングはtargetごとで行う。
-  var target = data.target;
-  if(_blockTailing[target] == true){
-    console.log("skip");
+  //無視ワード
+  var ignoreKeywords = config.get('ignore_keywords',[]);
+  var ignoreSkip = false;
+  ignoreKeywords.forEach(function(elm){
+    if(data.line.indexOf(elm) > -1){
+      ignoreSkip = true;
+      console.log("skip ignore word",elm,data.line);
+      return;
+    }
+  })
+  if(ignoreSkip){
     return;
   }
+  var highlightKeywords = config.get('highlight_keywords',[]);
+  highlightKeywords.forEach(function(elm){
+    if(data.line.indexOf(elm) > -1){
+      data.level = "error";
+    }
+  })
+  // 1秒程度notificationをブロックする。
+  // ブロッキングはtargetごとで行う。
+  io.sockets.emit('tail_raw', data); // ignore情報あたえたい
+  var target = data.target;
+  if(_blockTailing[target] == true){
+    //console.log("skip");
+    return;
+  }
+
   console.log(target, data);
   setBlock(target);
   io.sockets.emit('tail', data);
